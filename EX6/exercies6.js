bbc283fc6d7d756077601bf607c5ebefea49391d
Bekir Ozkan <chofficelch@gmail.com>
2022-11-24 22:45:00 +0100

    blog

--- EX6/exercies6.js
+++ EX6/exercies6.js
@@ -16,6 +16,27 @@ function updateCountdown(){
 }
 
 
+var i=0;
+
+var images= ["/images/4.jpg","/images/5.jpg","/images/6.jpg","/images/7.jpg","/images/7.jpg","/images/8.jpg"];
+var duration=2000;
+function slideImg(){
+
+    document.slide.src=images[i];
+
+    if(i<images.length-1){
+        i++;
+
+    }
+    else
+    {
+        
+    i=0;
+    }
+    setTimeout("slideImg()",duration);
+}
+
+window.onload=slideImg;
    
 
 

9ce0e1ec5764a972017c533e0a94320c32ca2484
Bekir Ozkan <chofficelch@gmail.com>
2022-11-29 10:31:16 +0100

    final

--- EX1/exercies1.js
+++
@@ -1,4 +0,0 @@
-const numbers=[1,2,3,4,5]
-let a=numbers.shift();
-numbers.push(a);
-console.log(numbers);//[2, 3, 4, 5, 1]
--- EX2/exercies2.js
+++
@@ -1,8 +0,0 @@
-//excercies 2
-const array=[1,2,"apple",4,"banana"]
-let sum="";
-for(let i=0; i<array.length;i++){
-    sum+=array[i]+","
-
-}
-console.log('"'+sum+'"');//"1,2,apple,4,banana," 
